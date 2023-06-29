9a555d1e72e238faf6f89bed1101b53f614fa3bd
micah elm <micah.b.elm@gmail.com>
2023-06-28 19:57:30 -0700

    move debug problem to own file

---
+++ src/components/DebugButton.jsx
@@ -0,0 +1,26 @@
+function ItemHold (){
+    const [buttonChild, setButtonChildren] = useState([1,2,3])
+  
+  function removeButton(removeId){
+    setButtonChildren(buttonChild.filter((id)=> id!== removeId))
+  }
+  
+    return (
+      <div>
+        <h1> have fun</h1>
+        {buttonChild.map((id)=>(
+          <Item key={id} id={id} remove={removeButton} />
+        ))}
+      </div>
+    )
+        }
+  
+  
+  function Item ({remove, id}){
+    return(
+      <div>
+        <button onClick={remove}>Fix me. id: {id}</button>
+      </div>
+    )
+  }
+  
